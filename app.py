4388080b716abc2b9abe670f677a81fb1dde2b61
Jason Vero <86158597+jasonvero@users.noreply.github.com>
2022-08-23 12:16:11 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,118 @@
+# --- Date: 8/23/2022
+# --- Project: Python Tutorial - Python Full Course for Beginners
+# --- Source: https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=WL&index=1&t=61s&ab_channel=ProgrammingwithMosh
+
+
+# --- Input
+
+# name = input('What is your name? ')
+# fav_color = input('What is your favorite color? ')
+# print(name + ' likes ' + fav_color)
+
+
+# --- Type Conversion
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2022 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# weight_lbs = input('What is your weight? (in pounds) ')
+# print(type(weight_lbs))
+# weight_kg = float(weight_lbs) * 0.453592
+# print(type(weight_kg))
+# print(weight_kg)
+
+# --- Strings
+
+# Examples when using double quote vs. single quote
+# course = "Python's Course for Beginners"
+# print(course)
+
+# course_2 = 'Python for "Beginners"'
+# print(course_2)
+
+# course_3 = '''
+# Hi John,
+# Here is our first email to you.
+#
+# Thank you,
+# '''
+# print(course_3)
+
+# course_4 = 'Python for Beginners'
+# print(course_4[0])  # index of first character
+# print(course_4[-1])  # index of last character
+# print(course_4[0:3])  # return first character to third character
+# print(course_4[0:])   # return all characters starting at first index
+# print(course_4[:5])   # return all characters up to fifth character
+#
+# another = course_4[:]  # return all characters from course_4 variable
+# print(another)
+
+# name = 'Jennifer'
+# print(name[1:-1])  # returns character starting from index one, all the way to end but excluding the last index
+
+
+# --- Formatted Strings
+
+# non-formatted string
+# first_name = 'John'
+# last_name = 'Smith'
+# message = first_name + ' [' + last_name + '] is a coder'
+# print(message)
+
+# formatted string
+# first_name = 'John'
+# last_name = 'Smith'
+# msg = f'{first_name} [{last_name}] is a coder'
+# print(msg)
+
+
+# --- String Methods
+# For example, upper() is a function that belongs to strings object exclusively, so it is a method
+# print() and len() are general purpose functions that are not specific to any type of object, so they are not methods
+
+course = 'Python for Beginners'
+print(len(course))
+course.upper()
+
+# --- Arithmetic Operations
+
+
+# --- Operator Precedence
+
+
+# --- Math Functions
+
+
+# --- If Statements
+
+
+# --- Logical operators
+
+
+# --- Weight Converter Program
+
+
+# --- While Loops
+
+
+# --- Building a Guessing Game
+
+# --- Building the Car Game
+
+
+# --- For Loops
+
+
+# --- Nested Loops
+
+
+# --- Lists
+
+
+# --- 2D Lists
+
+
